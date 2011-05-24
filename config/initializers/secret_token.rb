947bbd3a798d02e723dfadbf1e4988e022cc3404
Riccardo Cambiassi <riccardo@headshift.com>
2011-05-24 18:59:49 +0100

    created empty application SunspotBlog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunspotBlog::Application.config.secret_token = '4cd7094656860952bcbeb56b0672d742657a14b89eb2c0ed57178febcc122a3873834d4ce3203e495a961e5f49877774f5cc0650250032b88de99fcbe93b4a05'
